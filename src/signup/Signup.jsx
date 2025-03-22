5254f16528d2649fbdcaad84fc3a72235ac0035d
Elle <ellefamkar@gmail.com>
2025-03-22 23:43:49 +1100

    fix : Strengthen password check and confirmation

--- src/signup/Signup.jsx
+++ src/signup/Signup.jsx
@@ -3,10 +3,13 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
-  userName: "",
+  firstName: "",
+  lastName: "",
   email: "",
   password: "",
+  confirmPassword: "",
   phoneNumber: "",
+  successMsg: "",
 };
 
 const onSubmit = (values) => {
@@ -16,15 +19,31 @@ const onSubmit = (values) => {
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
+const passRegExp =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+
 const validationSchema = Yup.object().shape({
-  userName: Yup.string()
-    .min(2, "Name length is short.")
-    .max(50, "Name is too long!")
-    .required("Name is Required"),
+  firstName: Yup.string()
+    .min(2, "First name length is short.")
+    .max(50, "First name is too long!")
+    .required("First name is Required"),
+  lastName: Yup.string()
+    .min(2, "Family name length is short.")
+    .max(50, "Family name is too long!")
+    .required("Family name is Required"),
   email: Yup.string()
     .email("Invalid email Format")
     .required("Email is required"),
-  password: Yup.string().required("Password is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .matches(
+      passRegExp,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must be matched!"
+  ),
   phoneNumber: Yup.string()
     .matches(phoneRegExp, "Phone number is not valid")
     .nullable(),
@@ -42,15 +61,27 @@ function Signup() {
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div className="form-control">
-          <label htmlFor="userName">Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
-            {...formik.getFieldProps("userName")}
+            {...formik.getFieldProps("firstName")}
             type="text"
-            name="userName"
-            id="userName"
+            name="firstName"
+            id="firstName"
           />
-          {formik.errors.userName && formik.touched.userName && (
-            <p>{formik.errors.userName}</p>
+          {formik.errors.firstName && formik.touched.firstName && (
+            <p>{formik.errors.firstName}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            {...formik.getFieldProps("lastName")}
+            type="text"
+            name="lastName"
+            id="lastName"
+          />
+          {formik.errors.lastName && formik.touched.lastName && (
+            <p>{formik.errors.lastName}</p>
           )}
         </div>
         <div className="form-control">
@@ -75,6 +106,18 @@ function Signup() {
           />
           {formik.errors.password && formik.touched.password && (
             <p>{formik.errors.password}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            {...formik.getFieldProps("confirmPassword")}
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+          />
+          {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+            <p>{formik.errors.confirmPassword}</p>
           )}
         </div>
         <div className="form-control">
