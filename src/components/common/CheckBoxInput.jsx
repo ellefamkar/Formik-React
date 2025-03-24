62ade462117a97d98fcdf204953dc8f57227cfff
Elle <ellefamkar@gmail.com>
2025-03-24 13:11:07 +1100

    fix : Fix error handling in terms

--- src/components/common/CheckBoxInput.jsx
+++ src/components/common/CheckBoxInput.jsx
@@ -3,22 +3,35 @@ import React from "react";
 function CheckBoxInput({ checkBoxOptions, name, formik }) {
   return (
     <div className="mb-4">
-      {checkBoxOptions.map((item) => (
-        <React.Fragment key={item.value}>
-          <input
-            type="checkbox"
-            className="cursor-pointer"
-            id={item.value}
-            name={name}
-            value={item.value}
-            onChange={formik.handleChange}
-            checked={formik.values[name].includes(item.value)}
-          />
-          <label htmlFor={item.value} className="ml-1 mr-4 cursor-pointer">
-            {item.label}
-          </label>
-        </React.Fragment>
-      ))}
+      {checkBoxOptions.map((item) => {
+        const isChecked = formik.values[name].includes(item.value);
+
+        return (
+          <React.Fragment key={item.value}>
+            <input
+              type="checkbox"
+              className="cursor-pointer"
+              id={item.value}
+              name={name}
+              value={item.value}
+              checked={isChecked}
+              onChange = {(e) => {
+                let newValues = [...formik.values[name]];
+                if (e.target.checked) {
+                  newValues.push(item.value); 
+                } else {
+                  newValues = newValues.filter((val) => val !== item.value); // Remove unchecked value
+                }
+                formik.setFieldValue(name, newValues); 
+                formik.setTouched({ ...formik.touched, [name]: true }, false); // Mark as touched
+              }}
+            />
+            <label htmlFor={item.value} className="ml-1 mr-4 cursor-pointer">
+              {item.label}
+            </label>
+          </React.Fragment>
+        );
+      })}
       {formik.errors[name] && formik.touched[name] && (
         <p className="text-amber-600 mt-2 text-xs">{formik.errors[name]}</p>
       )}
